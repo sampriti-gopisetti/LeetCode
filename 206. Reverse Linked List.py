a3be76eac43b8bfa8243d0beee20f027c9f53ece
sampriti-gopisetti <sampriti@example.com>
2025-09-27 17:36:54 -0700

    Changes to 206

--- 206. Reverse Linked List.py
+++ 206. Reverse Linked List.py
@@ -4,7 +4,7 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]: # type: ignore
         if not head:
             return head
         list_val=[]

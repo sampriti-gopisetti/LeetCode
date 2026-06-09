7d766532ec9eaebc4711654cbdfdcf81252be003
sampriti-gopisetti <sampriti@example.com>
2026-06-08 21:47:01 -0700

    Updated 1.Two Sum

--- Arrays & Hashing/1. Two Sum.py
+++ Arrays & Hashing/1. Two Sum.py
@@ -1,24 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        index=[]
-        sorted_nums=sorted(nums)
-        start_index=0
-        last_index=len(nums)-1
-        mid_index=(start_index+last_index)/2
-        while True:
-            if sorted_nums[start_index]+sorted_nums[last_index]>target:
-                last_index=last_index-1
-            elif sorted_nums[start_index]+sorted_nums[last_index]<target:
-                start_index=start_index+1
-            elif sorted_nums[start_index]+sorted_nums[last_index]==target:
-                firstindex=nums.index(sorted_nums[start_index])
-                nums[nums.index(sorted_nums[start_index])]=''
-                index=[firstindex,nums.index(sorted_nums[last_index])]
-                return index
-            
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numbers={}
+        for i in range(0,len(nums)):
+            if nums[i] in numbers:
+                return [numbers[nums[i]],i]   
+            else:
+                numbers[target-nums[i]]=i;

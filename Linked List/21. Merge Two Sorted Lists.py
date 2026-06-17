5c610fb6619c8ee64b80a5ff9f09a3d99301e740
sampriti-gopisetti <sampriti@example.com>
2026-06-17 00:04:42 -0700

    21. Merge Two Sorted Lists - Update

--- Linked List/21. Merge Two Sorted Lists.py
+++ Linked List/21. Merge Two Sorted Lists.py
@@ -1,36 +1,26 @@
-#Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]: # type: ignore
-        if not list1:
-            return list2
-        elif not list2:
-            return list1
-        merged=ListNode()
-        if list1.val<list2.val:
-            merged.val=list1.val
-            list1=list1.next
-        else:
-            merged.val=list2.val
-            list2=list2.next
-        current=merged
-        while list1 and list2:
-            tempnode=ListNode()
-            if list1.val<list2.val:
-                tempnode.val=list1.val
-                current.next=tempnode
-                current=tempnode
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head=ListNode()
+        merged_list=ListNode()
+        head=merged_list
+        while(list1!=None and list2!=None):
+            node=ListNode()
+            if list1.val<=list2.val:
+                node.val=list1.val
+                merged_list.next=node
                 list1=list1.next
             else:
-                tempnode.val=list2.val
-                current.next=tempnode
-                current=tempnode
+                node.val=list2.val
+                merged_list.next=node
                 list2=list2.next
-        if list1:
-            current.next=list1
-        if list2:
-            current.next=list2
-        return merged
+            merged_list=merged_list.next
+        if (list1!=None):
+            merged_list.next=list1
+        elif (list2!=None):
+            merged_list.next=list2
+        return head.next

2fa7e0c973440b2af52a14c8383e7d495abeee46
sampriti-gopisetti <sampriti@example.com>
2026-06-23 11:20:07 -0700

    242. Valid Anagram - Update

--- Arrays & Hashing/242. Valid Anagram.py
+++ Arrays & Hashing/242. Valid Anagram.py
@@ -1,15 +1,16 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if len(s)==len(t):
-            for i in s:
-                t=t.replace(i,'',1)
-        if t=='':
-            return True
-        else:
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
             return False
-                
+        anagram={} 
+        for char in s:
+            if char in anagram:
+                anagram[char]+=1
+            else:
+                anagram[char]=1
+        for char in t:
+            if char in anagram and anagram[char]>0:
+                anagram[char]-=1
+            elif char not in anagram or anagram[char]<=0:
+                return False
+        return True

2b2dda85d5419a0e2439b783002aeac66f0fc595
sampriti-gopisetti <sampriti@example.com>
2026-06-24 19:59:32 -0700

    704. Binary Search - Updated

--- Binary Search/704. Binary Search.py
+++ Binary Search/704. Binary Search.py
@@ -1,13 +1,13 @@
 class Solution:
-    def search(self, nums: list[int], target: int) -> int:
-        low=0 
+    def search(self, nums: List[int], target: int) -> int:
+        low=0
         high=len(nums)-1
-        while low<=high:
-            mid=int((low+high)/2)
-            if target==nums[mid]:
+        while(low<=high):
+            mid=(low+high)//2
+            if nums[mid]==target:
                 return mid
-            elif target<nums[mid]:
-                high=mid-1
-            else:
+            elif nums[mid]<=target:
                 low=mid+1
+            else:
+                high=mid-1
         return -1
